8da1ebfd8345bf0481567024c48ed71949dfcc8e
Konstantin S. <havendv@gmail.com>
2026-09-13 19:46:13 +0400

    feat: Updated OpenAPI spec (#42)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Meshcapade/Generated/Meshcapade.CreditsClient.g.cs
+++ src/libs/Meshcapade/Generated/Meshcapade.CreditsClient.g.cs
@@ -34,7 +34,7 @@ namespace Meshcapade
         /// <inheritdoc/>
         public global::Meshcapade.AutoSDKClientOptions Options { get; }
 
-        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Meshcapade.SourceGenerationContext.Default);
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Meshcapade.CreditsSourceGenerationContext.Default);
 
         /// <summary>
         ///
